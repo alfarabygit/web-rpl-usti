5d289a72a835a0e4d684c1fc3553c683809a1d8c
Alfaraby Indra <alfarabyindra@gmail.com>
2025-08-13 22:00:09 +0700

    test navbar rpl usti responsive

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  toggle.addEventListener("click", () => {
+    //add show menu class to nav menu
+    nav.classList.toggle("show-menu");
+    //add show and hide menu icon
+    toggle.classList.toggle("show-icon");
+  });
+};
+
+showMenu("nav-toggle", "nav-menu");
