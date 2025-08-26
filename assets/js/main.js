0098aeea29178468f1e9fe109dd09f3636297f53
Alfaraby Indra <alfarabyindra@gmail.com>
2025-08-26 14:26:24 +0700

    upload change code web rpl usti

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
+const showMenu = (toogleId, navId) => {
+  const toggle = document.getElementById(toogleId),
     nav = document.getElementById(navId);
 
   toggle.addEventListener("click", () => {
@@ -12,3 +12,48 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu("nav-toggle", "nav-menu");
+
+/*=============== SWIPER HOME ===============*/
+const swiperHome = new Swiper(".home__swiper", {
+  loop: true,
+  slidesPerView: "auto",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+});
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+// const bgHeader = () => {
+//   const header = document.getElementById("header");
+
+//   this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header");
+// };
+// window.addEventListener("scroll", bgHeader);
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2800,
+  // reset: true,
+});
+
+sr.reveal(`.home__data, .home__swiper, .banner__container, .info__container, .form__container, .tik__container, .sisfo__container, .tifo__container, .footer__logo, .footer__data, .footer__rights`, {
+  origin: "top",
+  interval: 100,
+});
+
+sr.reveal(`.about__data `, {
+  origin: "left",
+  interval: 100,
+});
+
+sr.reveal(`.about__img-overlay`, {
+  origin: "right",
+  interval: 100,
+});
